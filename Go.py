bde39a6b5e01eea076ae0ae30d5adce7d6a4c674
Philip Clifton <philip@chameleonstudios.co.uk>
2013-05-16 12:05:23 +0100

    messages added

--- Go.py
+++ Go.py
@@ -16,11 +16,32 @@ class create_vhost(sublime_plugin.TextCommand):
 		self.giturl = string
 
 		if self.giturl != '' and self.folder != '':
-			print ('Creating VHOST at ' + self.folder + ' from  ' + self.giturl)
+			message = 'Creating virtual host at ' + self.folder + ' from  ' + self.giturl
 			print (subprocess.call("go create " + self.folder + ' ' + self.giturl , shell=True))
 		elif self.folder != '':
-			print ('Creating VHOST at ' + self.folder)
+			message = 'Creating virtual host ' + self.folder
 			print (subprocess.call("go create " + self.folder, shell=True))
+		else:
+			message = 'No project directory supplied'
+
+		self.output_view = self.get_window().get_output_panel("log")
+		self.output_view.set_read_only(False)
+		self._output_to_view(self.output_view, message, clear=True)
+		self.output_view.set_read_only(True)
+		self.get_window().run_command("show_panel", {"panel": "output.log"})
+
+	def _output_to_view(self, output_file, output, clear=False,
+	        syntax="Packages/Diff/Diff.tmLanguage", **kwargs):
+	    output_file.set_syntax_file(syntax)
+	    args = {
+	        'output': output,
+	        'clear': clear
+	    }
+	    output_file.run_command('git_scratch_output', args)
+
+	
+	def get_window(self):
+	    return self.view.window()
 
 	def project_change(self, string):
 		return ''
@@ -31,14 +52,32 @@ class create_vhost(sublime_plugin.TextCommand):
 class remove_vhost(sublime_plugin.TextCommand):
 
 	def run(self, edit):
-		# pprint.pprint(sublime.get_clipboard())
 		self.view.window().show_input_panel('Project Folder: ', '', self.project_done, self.project_change, self.project_cancel)
 		return ''
 
 	def project_done(self, string):
 		if string != '':
-			print ('Deleting VHOST at ' + string)
-			print (subprocess.call("go remove " + string, shell=True))
+			message = 'Deleting VHOST at ' + string
+			print (subprocess.Popen("go remove " + string, shell=True))
+
+			self.output_view = self.get_window().get_output_panel("log")
+			self.output_view.set_read_only(False)
+			self._output_to_view(self.output_view, message, clear=True)
+			self.output_view.set_read_only(True)
+			self.get_window().run_command("show_panel", {"panel": "output.log"})
+
+	def _output_to_view(self, output_file, output, clear=False,
+	        syntax="Packages/Diff/Diff.tmLanguage", **kwargs):
+	    output_file.set_syntax_file(syntax)
+	    args = {
+	        'output': output,
+	        'clear': clear
+	    }
+	    output_file.run_command('git_scratch_output', args)
+
+	
+	def get_window(self):
+	    return self.view.window()
 
 	def project_change(self, string):
 		return ''
@@ -50,14 +89,35 @@ class remove_vhost(sublime_plugin.TextCommand):
 class work_vhost(sublime_plugin.TextCommand):
 
 	def run(self, edit):
-		# pprint.pprint(sublime.get_clipboard())
 		self.view.window().show_input_panel('Project: ', '', self.project_done, self.project_change, self.project_cancel)
 		return ''
 
 	def project_done(self, string):
 		if string != '':
-			print ('Switching Project to ' + string)
-			print (subprocess.call("go work " + string, shell=True))
+			
+			if subprocess.Popen("go work " + string, shell=True):
+				message = 'Switching Project to ' + string
+			else:
+				message = 'Switch to Project failed'
+
+			self.output_view = self.get_window().get_output_panel("log")
+			self.output_view.set_read_only(False)
+			self._output_to_view(self.output_view, message, clear=True)
+			self.output_view.set_read_only(True)
+			self.get_window().run_command("show_panel", {"panel": "output.log"})
+
+	def _output_to_view(self, output_file, output, clear=False,
+	        syntax="Packages/Diff/Diff.tmLanguage", **kwargs):
+	    output_file.set_syntax_file(syntax)
+	    args = {
+	        'output': output,
+	        'clear': clear
+	    }
+	    output_file.run_command('git_scratch_output', args)
+
+	
+	def get_window(self):
+	    return self.view.window()
 
 	def project_change(self, string):
 		return ''
